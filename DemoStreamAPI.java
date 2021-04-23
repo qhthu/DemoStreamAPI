590a6c3fcdaa17434e9b971be22ccc2122d4dc97
qhthu <thuxyz7777@gmail.com>
2021-04-23 16:40:56 +0700

    addbyquach

--- DemoStreamAPI.java
+++ DemoStreamAPI.java
@@ -5,35 +5,53 @@
  */
 package java8;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author quach
  */
-interface Demo {
+public class DemoStreamAPI {
 
-    public void show();
+    public static void main(String[] args) throws Exception {
 
-}
+        Student s1 = new Student(1, "Nhung", 22, "female");
+        Student s2 = new Student(2, "An", 18, "male");
+        Student s3 = new Student(3, "Thu", 19, "female");
+        Student s4 = new Student(4, "Bao", 23, "male");
 
-interface Summary {
+        List<Student> student = Arrays.asList(s1, s2, s3, s4);
 
-    public void sum(int numA, int numB);
-}
+        List<Student> filterGender = student.stream().filter(st -> (st.getGender().equals("male"))).collect(Collectors.toList());
+        Iterator it = filterGender.iterator();
+        System.out.println("Nam sinh vien:");
+        System.out.printf("%-20s%-20s%-20s", "ID", "NAME", "AGE");
+        System.out.println();
+        while (it.hasNext()) {
+            Student next = (Student) it.next();
+            System.out.printf("%-20s%-20s%-20s", next.getId(), next.getName(), next.getAge());
+            System.out.println();
+        }
 
-public class DemoLambda{
+        int countAge = (int) student.stream().filter(st -> (st.getAge() >= 20)).count();
 
-    public static void main(String[] args) {
+        System.out.println("So sinh vien lon hon 20 tuoi la: " + countAge);
 
-        Demo d = () -> {
-            System.out.println("Quach Hong Thu!");
-        };
+        List<StudentDTO> studentDTOs = student.stream().map(s -> new StudentDTO(s.getId(), s.getName(), s.getAge(), s.getGender())).collect(Collectors.toList());
 
-        Summary s = (int numA, int numB) -> {
-            System.out.println(numA + numB);
-        };
-
-        s.sum(100, 28);
-        d.show();
+        int countAgeDTO = (int) studentDTOs.stream().filter(st -> (st.getAge() >= 20)).count();
+        Iterator i = studentDTOs.iterator();
+        System.out.println("Student DTO:");
+        System.out.printf("%-20s%-20s%-20s%-20s", "ID", "NAME", "AGE", "GENDER");
+        System.out.println();
+        while (i.hasNext()) {
+            StudentDTO next = (StudentDTO) i.next();
+            System.out.printf("%-20s%-20s%-20s%-20s", next.getId(), next.getName(), next.getAge(), next.getGender());
+            System.out.println();
+        }
     }
 
 }
---
+++ Student.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java8;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author quach
+ */
+class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Student(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
